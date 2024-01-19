1b8b3244c7534bd65aaf4906b0ed09f3d128bf63
meryemazzaim <zaimmaryame2@gmail.com>
2024-01-19 16:51:55 +0100

    section6

--- public/js/app.js
+++ public/js/app.js
@@ -14,25 +14,25 @@ button_break.addEventListener('click', () => {
     menu_lunch.style.display = 'none'
 })
 
-let myLogin = document.querySelector("#login")
-console.log(myLogin);
-let mySignup = document.querySelector("#signup")
-console.log(mySignup);
+// let myLogin = document.querySelector("#login")
+// console.log(myLogin);
+// let mySignup = document.querySelector("#signup")
+// console.log(mySignup);
 
-let myLoginContent = document.querySelector("#logincontent")
-console.log(myLoginContent);
-let mySignupContent = document.querySelector("#signupcontent")
-console.log(mySignupContent);
-myLogin.addEventListener('click', () => {
-    myLoginContent.classList.remove("d-none")
-    mySignupContent.classList.add("d-none")
-    console.log(myLoginContent);
-})
-mySignup.addEventListener('click', () => {
-    mySignupContent.classList.remove("d-none")
-    myLoginContent.classList.add("d-none")
-    console.log(mySignupContent);
-})
+// let myLoginContent = document.querySelector("#logincontent")
+// console.log(myLoginContent);
+// let mySignupContent = document.querySelector("#signupcontent")
+// console.log(mySignupContent);
+// myLogin.addEventListener('click', () => {
+//     myLoginContent.classList.remove("d-none")
+//     mySignupContent.classList.add("d-none")
+//     console.log(myLoginContent);
+// })
+// mySignup.addEventListener('click', () => {
+//     mySignupContent.classList.remove("d-none")
+//     myLoginContent.classList.add("d-none")
+//     console.log(mySignupContent);
+// })
 
 
 
