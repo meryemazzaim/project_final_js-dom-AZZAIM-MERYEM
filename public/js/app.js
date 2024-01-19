49c4c553e3e94dbc76e4e4fde1ab09ac49a2ef5c
meryemazzaim <zaimmaryame2@gmail.com>
2024-01-19 16:09:11 +0100

    menu

--- public/js/app.js
+++ public/js/app.js
@@ -14,11 +14,28 @@ button_break.addEventListener('click', () => {
     menu_lunch.style.display = 'none'
 })
 
-// let btn = document.querySelector('.btn')
-// let modal = document.querySelector('.modal')
-// btn.addEventListener('click', () => {
-//     modal.style.display = "block"
-// })
+let myLogin = document.querySelector("#login")
+console.log(myLogin);
+let mySignup = document.querySelector("#signup")
+console.log(mySignup);
+
+let myLoginContent = document.querySelector("#logincontent")
+console.log(myLoginContent);
+let mySignupContent = document.querySelector("#signupcontent")
+console.log(mySignupContent);
+myLogin.addEventListener('click', () => {
+    myLoginContent.classList.remove("d-none")
+    mySignupContent.classList.add("d-none")
+    console.log(myLoginContent);
+})
+mySignup.addEventListener('click', () => {
+    mySignupContent.classList.remove("d-none")
+    myLoginContent.classList.add("d-none")
+    console.log(mySignupContent);
+})
+
+
+
 
 
 
